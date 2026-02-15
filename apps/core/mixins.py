f00e62906c3f65edaa6af7b66057289490297d35
S1lenceY <u21216338@utp.edu.pe>
2026-02-15 08:56:39 -0500

    Adding Inactive Filters

--- apps/core/mixins.py
+++ apps/core/mixins.py
@@ -4,23 +4,29 @@ from rest_framework.response import Response
 
 class SoftDeleteModelViewSet(viewsets.ModelViewSet):
     """
-    Clase base inteligente para manejar el Borrado Lógico.
-    Cualquier ViewSet que herede de aquí ganará estos métodos automáticamente.
+    Clase base inteligente para manejar el Borrado Lógico y Filtrado de Estados.
     """
 
     def get_queryset(self):
-        # 1. Traemos el queryset base (ej: Sucursal.objects.all())
         queryset = super().get_queryset()
 
-        # 2. Si el frontend pide la lista completa, le damos solo los activos
         if self.action == 'list':
-            return queryset.filter(activo=True)
+            # 1. Capturamos el parámetro de la URL (si no lo envían, es None)
+            estado = self.request.query_params.get('estado', None)
 
-        # 3. Si busca por ID (para ver, editar o restaurar), le permitimos encontrarlo
+            # 2. Comparamos y filtramos según lo que pida el frontend
+            if estado == 'inactivo':
+                return queryset.filter(activo=False)
+            elif estado == 'todos':
+                return queryset  # Devuelve la tabla completa sin filtros
+            else:
+                # Comportamiento por defecto: Mostrar solo los activos
+                return queryset.filter(activo=True)
+
+        # Si busca por ID (para ver, editar o restaurar), le permitimos encontrarlo siempre
         return queryset
 
     def destroy(self, request, *args, **kwargs):
-        # El engaño perfecto: apagamos en lugar de borrar
         instancia = self.get_object()
         instancia.activo = False
         instancia.save()
