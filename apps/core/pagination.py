3c0982b50d0b353e75059d7379f9296532883e04
S1lenceY <u21216338@utp.edu.pe>
2026-03-21 00:27:29 -0500

    Adding Pagination for ALL ENDPOINTS

---
+++ apps/core/pagination.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+
+class PaginacionRetrocompatible(PageNumberPagination):
+    page_size = 20  # Por defecto traerá 20 registros por página
+    page_size_query_param = 'page_size'  # Permite al frontend pedir más (ej. ?page=1&page_size=50)
+    max_page_size = 100  # Límite de seguridad para que no saturen tu BD
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Si el frontend NO manda el parámetro '?page=' en la URL,
+        apagamos la paginación y devolvemos la lista plana para no romper nada.
+        """
+        if not request.query_params.get(self.page_query_param):
+            return None
+
+            # Si mandan '?page=', usamos el comportamiento normal de paginación de Django
+        return super().paginate_queryset(queryset, request, view)
