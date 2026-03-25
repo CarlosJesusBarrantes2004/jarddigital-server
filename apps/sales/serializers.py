1313dff2183c938ab8c353afbc2122cc3de818fb
Carlos Jesús Barrantes Saldarriaga <carlos2004jbs2307@gmail.com>
2026-03-24 20:43:54 -0500

    ..

--- apps/sales/serializers.py
+++ apps/sales/serializers.py
@@ -127,6 +127,20 @@ class VentaSerializer(serializers.ModelSerializer):
         read_only=True,
     )
 
+    distrito_nacimiento_nombre = serializers.CharField(
+        source="id_distrito_nacimiento.nombre", read_only=True, default=None
+    )
+    provincia_nacimiento_nombre = serializers.CharField(
+        source="id_distrito_nacimiento.id_provincia.nombre",
+        read_only=True,
+        default=None,
+    )
+    departamento_nacimiento_nombre = serializers.CharField(
+        source="id_distrito_nacimiento.id_provincia.id_departamento.nombre",
+        read_only=True,
+        default=None,
+    )
+
     # ---> NUEVO CAMPO <---
     ya_reingresada = serializers.SerializerMethodField(read_only=True)
 
