ae92b3e979ee8701a36ce5b3b3a4f3b742b961d6
Carlos Jesús Barrantes Saldarriaga <carlos2004jbs2307@gmail.com>
2026-03-31 08:26:51 -0500

    ..

--- apps/sales/filters.py
+++ apps/sales/filters.py
@@ -26,22 +26,32 @@ class VentaFilter(django_filters.FilterSet):
     id_estado_audios = django_filters.NumberFilter(field_name="id_estado_audios")
     id_producto = django_filters.NumberFilter(field_name="id_producto")
     id_origen_venta = django_filters.NumberFilter(field_name="id_origen_venta")
-    tecnologia = django_filters.CharFilter(field_name="tecnologia", lookup_expr="iexact")
+    id_sucursal = django_filters.NumberFilter(field_name="id_origen_venta__id_sucursal")
+    id_modalidad = django_filters.NumberFilter(
+        field_name="id_origen_venta__id_modalidad"
+    )
+    tecnologia = django_filters.CharFilter(
+        field_name="tecnologia", lookup_expr="iexact"
+    )
     es_full_claro = django_filters.BooleanFilter(field_name="es_full_claro")
 
     # ← CLAVE: permite ?solicitud_correccion=true para ventas EN CORRECCIÓN
-    solicitud_correccion = django_filters.BooleanFilter(field_name="solicitud_correccion")
+    solicitud_correccion = django_filters.BooleanFilter(
+        field_name="solicitud_correccion"
+    )
 
     class Meta:
         model = Venta
         fields = [
             "fecha_inicio",  # Añadido por orden visual
-            "fecha_fin",     # Añadido por orden visual
+            "fecha_fin",  # Añadido por orden visual
             "id_estado_sot",
             "id_sub_estado_sot",
             "id_estado_audios",
             "id_producto",
             "id_origen_venta",
+            "id_sucursal",
+            "id_modalidad",
             "tecnologia",
             "es_full_claro",
             "solicitud_correccion",
