e044d2161017e41fb64ffbe64b8026a365fab9c4
S1lenceY <u21216338@utp.edu.pe>
2026-04-29 01:07:45 -0500

    Updating Tracking Models

--- apps/tracking/models.py
+++ apps/tracking/models.py
@@ -3,7 +3,7 @@ from apps.sales.models import Venta
 
 
 class Seguimiento(models.Model):
-    # Definimos el ENUM al estilo Django
+    # Definimos el ENUM
     ESTADO_CHOICES = [
         ('PENALIZADO', 'Penalizado'),
         ('SUSPENDIDO', 'Suspendido'),
@@ -12,13 +12,13 @@ class Seguimiento(models.Model):
 
     # Relación 1 a 1: Una venta tiene un único registro maestro de seguimiento
     id_venta = models.OneToOneField(
-        Venta,  # Asegúrate de tener importado el modelo Venta
+        Venta,
         on_delete=models.CASCADE,
         related_name="seguimiento",
         db_column="id_venta"
     )
     codigo_pago = models.CharField(max_length=50, null=True, blank=True)
-    ciclo_facturacion = models.CharField(max_length=20, null=True, blank=True)
+    ciclo_facturacion = models.DateField(null=True, blank=True)
     fecha_inicio = models.DateField(null=True, blank=True)
     activo = models.BooleanField(default=True)
 
@@ -26,16 +26,22 @@ class Seguimiento(models.Model):
     estado = models.CharField(
         max_length=20,
         choices=ESTADO_CHOICES,
-        null=True,  # Permitimos null para no afectar los registros existentes si los hubiera
+        null=True,
         blank=True
     )
-    descuento_realizado = models.BooleanField(default=False)  # default=False es el escudo protector
+    descuento_realizado = models.BooleanField(default=False)
 
     class Meta:
         db_table = "seguimiento"
 
 
 class SeguimientoMensual(models.Model):
+    # ---> NUEVO ENUM PARA CONFORMIDAD <---
+    CONFORMIDAD_CHOICES = [
+        ('CONFORME', 'Conforme'),
+        ('INCONFORME', 'Inconforme'),
+    ]
+
     id_seguimiento = models.ForeignKey(
         Seguimiento,
         on_delete=models.CASCADE,
@@ -46,11 +52,20 @@ class SeguimientoMensual(models.Model):
     pago_cliente_realizado = models.BooleanField(default=False)
 
     # ---> NUEVO CAMPO INYECTADO <---
-    fecha_seguimiento = models.DateField(null=True, blank=True)  # null=True evita bloqueos en migraciones
+    fecha_seguimiento = models.DateField(null=True, blank=True)
 
     fecha_validacion_pago = models.DateField(null=True, blank=True)
     observacion = models.TextField(null=True, blank=True)
-    activo = models.BooleanField(default=True)  # ¡Tu salvavidas original se mantiene!
+
+    # ---> NUEVO CAMPO INYECTADO <---
+    conformidad = models.CharField(
+        max_length=20,
+        choices=CONFORMIDAD_CHOICES,
+        null=True,
+        blank=True
+    )
+
+    activo = models.BooleanField(default=True)
 
     class Meta:
         db_table = "seguimiento_mensual"
